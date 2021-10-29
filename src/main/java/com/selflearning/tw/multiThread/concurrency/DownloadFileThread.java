7066cc64aa3ab5c6a575c5f36e097b4cebf1e821
Jun-Lin_Chen <Jun-Lin_Chen@syscom.com.tw>
2021-10-29 17:59:14 +0800

    Pausing a thread

--- src/main/java/com/selflearning/tw/multiThread/concurrency/DownloadFileThread.java
+++ src/main/java/com/selflearning/tw/multiThread/concurrency/DownloadFileThread.java
@@ -4,5 +4,12 @@ public class DownloadFileThread implements Runnable {
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Download complete: " + Thread.currentThread().getName());
     }
 }
