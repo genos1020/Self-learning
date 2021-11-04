d36ac80721903d0c908214c7d4164eb1ad434e95
Jun-Lin_Chen <Jun-Lin_Chen@syscom.com.tw>
2021-11-04 13:40:36 +0800

    加上註解說明

--- src/main/java/com/selflearning/tw/multiThread/executors/CompletableFuturesDemo.java
+++ src/main/java/com/selflearning/tw/multiThread/executors/CompletableFuturesDemo.java
@@ -2,63 +2,95 @@ package com.selflearning.tw.multiThread.executors;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
+// asynchronous
 public class CompletableFuturesDemo {
-    public static void show(){
-        Supplier<Integer> task = () -> 1;
-        var future = CompletableFuture.supplyAsync(task); // non-blocking
+    public static void show() {
+        // 如果沒有指定executor的話，預設為ForkJoinPool.commonPool()
+        // commonPool是根據系統的可用thread( 每core為2個thread ) Runtime.getRuntime().availableProcessors()
+        // 產生該數量的thread到pool中，e.g. 本機為 2 core = 4 thread
+        var executor = Executors.newFixedThreadPool(100);
+        var withExecutorFuture = CompletableFuture
+                                                    .supplyAsync(() -> {
+                                                        LongTask.simulate();
+                                                        System.out.println(Thread.currentThread().getName());
+                                                        return "with fixed thread pool executor";
+                                                    }, executor);
+
+        // non-blocking
+        // thenAccept => main thread / thenAcceptAsync => ForkJoinPool.commonPool-worker-7 (另外的thread中執行)
+//        withExecutorFuture.thenAcceptAsync((q)-> {
+//            System.out.println(Thread.currentThread().getName());
+//            System.out.println(q);
+//        }); // 2
+//        System.out.println("lez go"); // 1
+
+        // blocking
         try {
-            Integer result = future.get();
-            System.out.println(result);
+            System.out.println(Thread.currentThread().getName());
+            System.out.println(withExecutorFuture.get()); // 1
+            System.out.println("lez go"); // 2
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
+
+        Supplier<Integer> task = () -> 1;
+        var future = CompletableFuture.supplyAsync(task); // non-blocking
+        try {
+            Integer result = future.get(); // block
+            System.out.println(result);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
     }
 
-    // Composing Completable Futures
-    public static void composingCompletableFutures(){
+    // Composing Completable Futures: 第一個操作完成時，將其結果作為參數傳遞給第二個操作。
+    public static void composingCompletableFutures() {
 
         getUserEmailAsync()
                 .thenCompose(CompletableFuturesDemo::getPlaylistAsync)
                 .thenAccept(p -> System.out.println(p));
     }
 
-    public static CompletableFuture<String> getUserEmailAsync(){
+    public static CompletableFuture<String> getUserEmailAsync() {
         return CompletableFuture.supplyAsync(() -> "email");
     }
 
-    public static CompletableFuture<String> getPlaylistAsync(String email){
+    public static CompletableFuture<String> getPlaylistAsync(String email) {
         return CompletableFuture.supplyAsync(() -> "playlist");
     }
 
 
     // Combining Completable Futures
-    public static void combineCompletableFuture(){
+    public static void combineCompletableFuture() {
         var first = CompletableFuture
-                                                .supplyAsync(()-> "20USD")
-                                                .thenApply(str -> {
-                                                    var price = str.replace("USD", "");
-                                                    return Integer.parseInt(price);
-                                                });
-        var second = CompletableFuture.supplyAsync(()-> 0.9);
-        first.thenCombine(second, (price, exchageRate) -> price*exchageRate)
+                .supplyAsync(() -> "20USD")
+                .thenApply(str -> { // thenApply => 類似 .map 負責改變型態
+                    var price = str.replace("USD", "");
+                    return Integer.parseInt(price);
+                });
+        var second = CompletableFuture.supplyAsync(() -> 0.9);
+        first.thenCombine(second, (price, exchageRate) -> price * exchageRate)
                 .thenAccept(result -> System.out.println(result));
     }
 
 
     // Waiting for Many Tasks to Complete
-    public static void waitForManyTasks(){
+    public static void waitForManyTasks() {
         var first = CompletableFuture.supplyAsync(() -> 1);
         var second = CompletableFuture.supplyAsync(() -> 2);
         var third = CompletableFuture.supplyAsync(() -> 3);
         var all = CompletableFuture.allOf(first, second, third);
-        all.thenRun(()->{
+        all.thenRun(() -> {
             try {
-              var firstResult = first.get();
-              var secondResult = second.get();
-              var thirdResult = third.get();
+                var firstResult = first.get();
+                var secondResult = second.get();
+                var thirdResult = third.get();
                 System.out.println(firstResult);
                 System.out.println(secondResult);
                 System.out.println(thirdResult);
@@ -72,7 +104,7 @@ public class CompletableFuturesDemo {
 
 
     // Handling timeouts
-    public static void handleTimeouts(){
+    public static void handleTimeouts() {
         var future = CompletableFuture.supplyAsync(() -> {
             LongTask.simulate();
             return 1;
--- src/main/java/com/selflearning/tw/multiThread/executors/ExecutorsDemo.java
+++ src/main/java/com/selflearning/tw/multiThread/executors/ExecutorsDemo.java
@@ -1,15 +1,14 @@
 package com.selflearning.tw.multiThread.executors;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class ExecutorsDemo {
 
     public static void show(){
+        // Executors 方便管理thread，但無法避免 concurrency problem (兩個thread 同時改動到同一個資源)
         var executor = Executors.newFixedThreadPool(2);
         System.out.println(executor.getClass().getName());
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(100);
 
         try{
             var future = executor.submit(() -> {
@@ -21,6 +20,7 @@ public class ExecutorsDemo {
             System.out.println("Do more and more work");
             try {
                 var result = future.get(); // blocking method: 會在這等到thread task執行完成 才會往下繼續執行
+//                future.get(10, TimeUnit.SECONDS); // 設定timeout 10 sec
                 System.out.println(result);
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
